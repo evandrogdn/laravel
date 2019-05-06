d09d85900b468970e5be58eea07dbf6f92d37f03
Bruna Klaumann <39775548+BrunaKlaumann@users.noreply.github.com>
2019-05-06 20:43:09 -0300

    [UPD] Menu de navegação

--- resources/views/regiao/formulario.blade.php
+++ resources/views/regiao/formulario.blade.php
@@ -1,6 +1,23 @@
 @extends("principal")
 
 @section("conteudo")
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div  class="container">
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent">
+        Menu
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li><a href="/fornecedores" class="nav-link"> Listagem  dos Fornecedores </a></li>
+          <li><a href="/pessoas" class="nav-link"> Listagem das Pessoas </a></li>
+          <li><a href="/produtos" class="nav-link"> Listagem dos Produtos</a></li>
+          <li><a href="/regiao" class="nav-link"> Listagem das regioes</a></li>
+          <li><a href="/transportadoras" class="nav-link"> Listagem das transportadoras</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
 <form method="POST" action="/regiao/gravar">
     @method('POST')
     @csrf
